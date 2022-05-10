ecdf6a7299c5228ab5a3f768d22f170c02f0c178
raul reymundi <kinskispiral@gmail.com>
2022-05-10 16:14:05 -0400

    commit tree 1b6ae4c3384dea420baf83db2bbe096840aed665

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(`Ralph`);
+};
+
+function destructivelyPrependCat(name){
+    cats.unshift(`Bob`);
+};
+
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+};
+
+const catsCopy = [...cats];
+
+function appendCat(name){
+    return [...catsCopy, `Broom`];
+};
+
+function prependCat(name){
+    return [`Arnold`, ...catsCopy];
+};
+
+//const catsAppend = [...cats, `Broom`];
+
+//function appendCat(name){
+   // return catsAppend;
+//}
+
+//const catsPrepend = [`Arnold`, ...cats];
+
+//function prependCat(name){
+   // return catsPrepend;
+//}
+
+function removeLastCat(){
+    return catsCopy.slice(0, -1);
+};
+
+function removeFirstCat(){
+    return catsCopy.slice(1);
+};
